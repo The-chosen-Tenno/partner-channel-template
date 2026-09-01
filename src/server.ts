60d2519eac26407677bbbf6066ff2b6172c81a80
mohamed sadhir <xempforever@gmail.com>
2026-09-01 11:17:01 +0530

    Use environment variable for server port

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ export const frontId = '3139298cca9bb76d'; // The App UID of your Front app
 export const frontSecret = '23a25a0df75364d4574467e35b2a035b';
 export const frontUrl = 'https://api2.frontapp.com'; // This URL subdomain might be different for your company
 export const callbackHostname = 'https://partner-channel-template-production-7318.up.railway.app';
-export const serverPort = '3000';
+export const serverPort = process.env.PORT || '3000';
 
 export function randomString(length: number): string {
   return randomBytes(Math.floor(length / 2)).toString('hex');
