cf4a42a51e02dc0f11277cc3d3776b577b9593df
mohamed sadhir <xempforever@gmail.com>
2026-09-07 12:38:44 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 
 export const frontId = process.env.FRONT_ID as string;
 export const frontSecret = process.env.FRONT_SECRET as string;
+export const frontUrl = 'https://api2.frontapp.com';
 export const callbackHostname = process.env.CALLBACK_HOSTNAME as string;
 export const channelId = process.env.CHANNEL_ID as string;
 export const serverPort = process.env.PORT || '3000';
